915c054a4fb7ac9976404fb512c554e332c09349
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-13 14:52:44 +0530

    about page created

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,10 +1,16 @@
-import Link from "next/link";
+import WhoIAm from "../components/about/whoIAm";
 
 const About = () => {
   return (
     <>
-      <h2>About page</h2>
-      <Link href="/">ho</Link>
+      <div className="px-4 pt-20 pb-4 bg-[url('/RoboticBG.png')] bg-slate-900 bg-blend-overlay bg-no-repeat bg-cover bg-right">
+        <div>
+          <h2 className="text-3xl font-bold text-sky-500 text-center pb-20">
+            Who am I?
+          </h2>
+          <WhoIAm />
+        </div>
+      </div>
     </>
   );
 };
