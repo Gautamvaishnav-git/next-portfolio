4d8fbd00a064680bdd7fd7eea1d465891d6eeef9
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-14 15:55:28 +0530

    robotic bg removed

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,9 +3,9 @@ import WhoIAm from "../components/about/whoIAm";
 const About = () => {
   return (
     <>
-      <div className="px-4 pt-20 pb-4 bg-[url('/RoboticBG.png')] bg-slate-900 bg-blend-overlay bg-no-repeat bg-cover bg-right">
+      <div className="px-4 pt-20 pb-4 bg-slate-900 bg-blend-overlay bg-no-repeat bg-cover bg-right">
         <div>
-          <h2 className="text-3xl font-bold text-sky-500 text-center pb-20">
+          <h2 className="sm:text-5xl text-3xl font-bold text-sky-500 text-center pb-20">
             Who am I?
           </h2>
           <WhoIAm />
