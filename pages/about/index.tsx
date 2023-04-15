a8cd2810fd3c0cc2e196e6dd50150a94d052bd4e
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-15 16:04:13 +0530

    bg added in about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,7 +3,7 @@ import WhoIAm from "../components/about/whoIAm";
 const About = () => {
   return (
     <>
-      <div className="px-4 pt-20 pb-4 bg-slate-900 bg-blend-overlay bg-no-repeat bg-cover bg-right">
+      <div className="sm:px-4 px-2 pt-20 pb-4 bg-slate-900 bg-[url('/aboutBG.webp')] bg-no-repeat bg-cover">
         <div>
           <h2 className="sm:text-5xl text-3xl font-bold text-sky-500 text-center pb-20">
             Who am I?
