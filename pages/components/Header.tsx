023509e3255937a24523f0658cdc15744b49ccb0
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2023-04-15 16:05:13 +0530

    resume linked

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -9,7 +9,11 @@ const Header = () => {
         <div className="xl:container m-auto px-6 md:px-12 lg:px-6">
           <div className="flex flex-wrap items-center justify-between gap-6 md:py-3 md:gap-0 lg:py-5">
             <div className="w-full items-center flex justify-between lg:w-auto">
-              <Link href="/" className="relative text-3xl text-white font-bold z-10" aria-label="logo">
+              <Link
+                href="/"
+                className="relative text-3xl text-white font-bold z-10"
+                aria-label="logo"
+              >
                 Gautam
               </Link>
               <label
@@ -74,7 +78,9 @@ const Header = () => {
 
               <div className="w-full space-y-2 border-primary/10 dark:border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l">
                 <a
-                  href="#"
+                  href="./resume.pdf"
+                  target="_blank"
+                  download
                   className="relative flex h-9 ml-2 items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full before:bg-sky-600 dark:before:bg-sky-400 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
                 >
                   <span className="relative text-sm font-semibold text-white dark:text-gray-900">
